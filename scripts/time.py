0dfc24cb69eded233c39b80bc9d41215c93f889e
BrandonWerbel <56608573+BrandonWerbel@users.noreply.github.com>
2025-04-09 19:14:46 -0400

    Working average tree length and distance

--- scripts/time.py
+++ scripts/time.py
@@ -14,6 +14,7 @@ NA = "NA" # what to print when function is not implemented
 from io import StringIO
 from sys import argv
 from time import time
+import re
 if len(argv) != 4 or argv[2] not in {'treeswift','dendropy','biophylo','ete3', 'networkx'}:
     print("USAGE: %s <tree_file> <treeswift_or_dendropy_or_biophylo_or_ete3> <task>"%argv[0]); exit(1)
 if argv[1].lower().endswith('.gz'):
@@ -32,13 +33,13 @@ def avg_tree_length(m):
     elif m == 'biophylo':
         tree = Phylo.read(treeio, 'newick')
         t_start = time()
-        numpy.average(branch_lengths = [clade.branch_length for clade in tree.find_clades() if clade.branch_length])
+        numpy.average([clade.branch_length for clade in tree.find_clades() if clade.branch_length])
         t_end = time()
     elif m == 'networkx':
         tree = Phylo.read(treeio, 'newick')
         G = Phylo.to_networkx(tree)
         t_start = time()
-        numpy.average(branch_lengths = [data["weight"] for _, _, data in tree.edges(data=True)])
+        numpy.average([data["weight"] for _, _, data in G.edges(data=True)])
         t_end = time()
     elif m == 'treeswift':
         tree = read_tree_newick(treestr)
@@ -128,48 +129,40 @@ def treeness(m):
     return t_end-t_start
 
 def distance(m):  # Alexis - this is complete I beilieve let me know if there are any issues
+    matches = re.findall(r"L\d+", treestr)
+    taxon1, taxon2 = random.choice(matches), random.choice(matches)
+    
     if m == 'dendropy':
         tree = dendropy.Tree.get(data=treestr, schema='newick')
-        root = tree.seed_node
-        random_node = random.choice(tree.nodes())
-        node1, node2 = root, random_node        
+        taxon1, taxon2 = random.choice(tree.taxon_namespace), random.choice(tree.taxon_namespace)    
         t_start = time()
-        tree.PhylogeneticDistanceMatrix.distance(node1, node2)
+        tree.phylogenetic_distance_matrix().distance(taxon1, taxon2)
         t_end = time()
     elif m == 'biophylo':
         tree = Phylo.read(treeio, 'newick')
-        root = tree.root
-        all_nodes = list(tree.get_terminals()) + list(tree.get_nonterimnals())
-        random_node = random.choice(all_nodes)
-        node1, node2 = root, random_node
+        node1, node2 = list(tree.find_clades(name=taxon1))[0], list(tree.find_clades(name=taxon2))[0]
         t_start = time()
-        tree.distance(node1, node2)
+        tree.distance(taxon1, taxon2)
         t_end = time()
     elif m == 'networkx':
         tree = Phylo.read(treeio, 'newick')
         G = Phylo.to_networkx(tree)
-        root = tree.root
-        all_nodes = list(G.nodes())
-        random_node = random.choice(nodes)
-        node1, node2 = root, random_node
+        node1 = next(node for node in G if taxon1 == str(node))
+        node2 = next(node for node in G if taxon2 == str(node))
         t_start = time()
         networkx.shortest_path_length(G, source=node1, target=node2)
         t_end = time()
     elif m == 'treeswift':
         tree = read_tree_newick(treestr)
-        root = tree.root
-        all_nodes = list(tree.get_nodes())
-        random_node = random.choice(all_nodes)
-        node1, node2 = root, random_node
+        node1 = tree.find_node(taxon1, True, True)
+        node2 = tree.find_node(taxon2, True, True)
         t_start = time()
         tree.distance_between(node1, node2)
         t_end = time()
     elif m == 'ete3':
         tree = ete3.Tree(treestr, format=1)
-        root = tree.get_tre_root()
-        all_nodes = tree.get_descendants()
-        random_node = random.choice(all_nodes)
-        node1, node2 = root, random_node
+        node1 = tree.search_nodes(name=taxon1)[0]
+        node2 = tree.search_nodes(name=taxon2)[0]
         t_start = time()
         node1.get_distance(node2)
         t_end = time()
